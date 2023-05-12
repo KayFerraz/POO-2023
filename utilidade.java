e503ed190cf3d433404ccc52f77bc23455f2df5b
Kay Ferraz <kayanne.pf.souza@unesp.br>
2023-05-12 16:35:33 -0300

    Create utilidade.java

---
+++ utilidade.java
@@ -0,0 +1,17 @@
+public class Utilidade {
+public static int max (int n1,int n2){
+if (n1>n2) 
+return n1;
+return n2;}
+
+
+public static double int (double n1, double n2){
+    if (n1>n2) 
+       return n1;
+    return n2;}
+
+public static utilidade{
+  public static int max (int n1, int n2){
+      if (n1>n2)
+        return n1;
+      return n2;
