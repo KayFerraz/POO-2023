c04acf95707efc114a44ff1475c35fc6edbad6f2
Kay Ferraz <kayanne.pf.souza@unesp.br>
2023-05-12 16:40:52 -0300

    Create principal.java

---
+++ principal.java
@@ -0,0 +1,6 @@
+ https://docs.oracle.com/javase/8/docs/api/javax/swing/JOptionPane.html 
+//KAY FERRAZ 
+//CIENCIA DA COMPUTAÇÃO FCT UNESP - PROGRAMAÇÃO ORIENTADA A OBJETOS
+
+aux=JOptionPane.showInputDialog("digite um valor"); //retorna um valor, uma string
+ valor = double.parseDouble(aux); //vem o valor armazenado com uma string e armazena variavel double
